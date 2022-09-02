9b056e6eace4e9202037a12dff01dcb4d231fb70
Yannic Haas <yannic.haas@spie.com>
2022-09-02 09:53:37 +0200

    Inital

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getHeight () {
+    // var imgHeight = document.querySelectorAll('.imgHeight');
+    // var hoverOverlay = document.querySelectorAll('.hoverOverlay');
+
+    // for (i = 0; i < hoverOverlay.length; i++) {
+    //     var height = imgHeight[i].clientHeight;
+    //     var realHeight = height + "px";
+    //     hoverOverlay[i].style.height = realHeight;
+    // } 
+}
+
+// window.addEventListener('resize', function setHeight() {    
+//     var imgHeight = document.querySelectorAll('.imgHeight');
+//     var hoverOverlay = document.querySelectorAll('.hoverOverlay');
+
+//     for (i = 0; i < hoverOverlay.length; i++) {
+//         var height = imgHeight[i].clientHeight;
+//         var realHeight = height + "px";
+//         hoverOverlay[i].style.height = realHeight;
+//     } 
+// })
